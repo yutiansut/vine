e5e99eb7c281e7bbfa9d2823aecda672bcf6b23b
Ask Solem <ask@celeryproject.org>
2016-04-21 13:21:58 -0700

    Adds vine.abstract.ThenableProxy for objects that references a promise

--- vine/abstract.py
+++ vine/abstract.py
@@ -37,3 +37,34 @@ class Thenable(Callable):  # pragma: no cover
         # overide to return other so `register` can be used as a decorator
         type(cls).register(cls, other)
         return other
+
+
+@Thenable.register
+class ThenableProxy(object):
+
+    def _set_promise_target(self, p):
+        self._p = p
+
+    def then(self, on_success, on_error=None):
+        return self._p.then(on_success, on_error)
+
+    def cancel(self):
+        return self._p.cancel()
+
+    def throw1(self, exc=None):
+        return self._p.throw1(exc)
+
+    def throw(self, exc=None, tb=None, propagate=True):
+        return self._p.throw(exc, tb=tb, propagate=propagate)
+
+    @property
+    def cancelled(self):
+        return self._p.cancelled
+
+    @property
+    def ready(self):
+        return self._p.ready
+
+    @property
+    def failed(self):
+        return self._p.failed
