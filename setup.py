aa47e644baf3d413387e9348a7c189df8fa14dd1
Jon Dufresne <jon.dufresne@gmail.com>
2017-05-23 22:29:13 -0700

    Document and add testing support for Python 3.6 (#14)

--- setup.py
+++ setup.py
@@ -22,6 +22,10 @@ classes = """
     Programming Language :: Python :: 2.7
     Programming Language :: Python :: 3
     Programming Language :: Python :: 3.4
+    Programming Language :: Python :: 3.5
+    Programming Language :: Python :: 3.6
+    Programming Language :: Python :: Implementation :: CPython
+    Programming Language :: Python :: Implementation :: PyPy
     License :: OSI Approved :: BSD License
     Intended Audience :: Developers
     Operating System :: OS Independent
